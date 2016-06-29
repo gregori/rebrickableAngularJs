26ce84a00cadf24ef5ce11b482e72950613bb6f2
Rodrigo Habib Gregori <rodrigo.gregori@joinville.sc.gov.br>
2016-06-29 17:24:30 -0300

    Adicionadas funcoes para pegar tipos

--- js/rebrickable.service.js
+++ js/rebrickable.service.js
@@ -5,33 +5,68 @@
         .module('app')
         .factory('RebrickableService', RebrickableService);
 
-    RebrickableService.$inject = ['$http'];
+    RebrickableService.$inject = ['$http', '$q'];
 
-    function RebrickableService($http) {
+    function RebrickableService($http, $q) {
         var url = 'https://rebrickable.com/api';
-
-        return {
-            getSets: getSets
+        var params = {
+            key: 'EWAKwlOdek',
+            format: 'json'
         };
 
-        function getSets(queryStr) {
-            return $http.get(url + '/search', {
-                params: { 
-                    key: 'EWAKwlOdek',
-                    format: 'json',
-                    type: 'S',
-                    query: queryStr
-                }  
-            })
-                .then(getSetsComplete)
-                .catch(getSetsFailed);
+        var types = [
+            {id: 'S', name: 'Set'},
+            {id: 'M', name: 'MOC'},
+            {id: 'P', name: 'Part'}
+        ];
 
-            function getSetsComplete(response) {
-                return response.data.results;
+
+        return {
+            getSets: getSets,
+            getSet: getSet,
+            getPartTypes: getPartTypes,
+            getTypes: getTypes
+        };
+
+        function getTypes() {
+            return function() {
+                var defer = $q.defer();
+                defer.resolve(types);
+                return defer.promise;
             }
-            function getSetsFailed(error) {
-                console.error('Erro ao buscar conjuntos: ' + error.data);
+        }
+
+        function getResults(uri, params) {
+            return $http.get(url + uri, {params: params})
+                .then(getResultsComplete)
+                .catch(getResultsFailed);
+
+            function getResultsComplete(response) {
+                return response.data;
             }
+            function getResultsFailed(error) {
+                console.error('Erro ao buscar: ' + error.data);
+            }
+        }
+
+        function getPartTypes() {
+            return getResults('/get_part_types', params);
+        }
+
+        function getSets(queryStr, partType) {
+            var _params = params;
+            _params.type = 'S';
+            _params.query = queryStr;
+            _params.part_type_id = partType;
+            console.log(_params);
+            return getResults('/search', _params);
+        }
+
+        function getSet(id) {
+            var _params = params;
+            _params.set_id = id;
+
+            return getResults('/get_set', _params);
         }
     }
 
--- js/sets.controller.js
+++ js/sets.controller.js
@@ -11,15 +11,38 @@
             var vm = this;
             vm.search = "";
             vm.sets = [];
+            vm.partTypes = [];
+            vm.types = [];
             vm.getSets = getSets;
 
-            function getSets() {
-                return RebrickableService.getSets(vm.search)
+            getPartTypes();
+            getTypes();
+
+            function getTypes() {
+                return RebrickableService.getTypes()
                     .then(function(data) {
-                        vm.sets = data;
+                        vm.types = data;
+                        return vm.types;
+                    });
+            }
+
+            function getSets() {
+                var id = typeof vm.partType !== 'undefined' ? vm.partType.part_type_id : ''; 
+                return RebrickableService.getSets(vm.search, id)
+                    .then(function(data) {
+                        vm.sets = data.results;
                         return vm.sets;
                     });
-           } 
+            } 
+
+            function getPartTypes() {
+                return RebrickableService.getPartTypes()
+                    .then(function(data) {
+                        vm.partTypes = data.part_types;
+                        return vm.partTypes;
+                    });
+            }
+
         }
 
 })();
